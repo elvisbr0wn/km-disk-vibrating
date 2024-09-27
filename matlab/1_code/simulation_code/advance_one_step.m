5e6c1c0651c1bd31d3404695f13d2b3a77ea3642
Elvis Aguero <elvisavfc65@gmail.com>
2024-09-27 08:48:56 -0400

    saving precomputed inverse

--- matlab/1_code/simulation_code/advance_one_step.m
+++ matlab/1_code/simulation_code/advance_one_step.m
@@ -1,39 +1,50 @@
-function next_condition = advance_one_step(previous_conditions, PROBLEM_CONSTANTS)
+function [next_condition, PROBLEM_CONSTANTS] = advance_one_step(previous_conditions, PROBLEM_CONSTANTS)
 
-    % If the matrix exists, import it
-
-    % If it does not, create it and then save it
     dt = previous_conditions.dt;
-    We = PROBLEM_CONSTANTS.weber;
-    Re = PROBLEM_CONSTANTS.reynolds;
     Fr = PROBLEM_CONSTANTS.froude;
-    Delta = PROBLEM_CONSTANTS.laplacian;
-    DTN = PROBLEM_CONSTANTS.DTN;
+    F = PROBLEM_CONSTANTS.force_amplitude;
+    w = PROBLEM_CONSTANTS.force_frequency;
+    t = previous_conditions.time;
     nr = PROBLEM_CONSTANTS.nr;
     cPoints = PROBLEM_CONSTANTS.contact_points;
-    pIntegral = PROBLEM_CONSTANTS.pressure_integral;
-    Ma = PROBLEM_CONSTANTS.obj_mass;
-    F = PROBLEM_CONSTANTS.force_amplitude;
-    t = previous_conditions.time + dt;
-    w = PROBLEM_CONSTANTS.force_frequency;
     CoM = previous_conditions.center_of_mass;
     CoM_vel = previous_conditions.center_of_mass_velocity;
-    % Preparing the matrix (2x2 block)
-    Sist = [[eye(nr)-dt*2*Delta/Re,-dt*DTN];...
-        [dt*(eye(nr)/Fr-Delta/We),eye(nr)-dt*2*Delta/Re]];
-      
-    % Completing the system
-    Mat =  [[Sist(:,(cPoints+1):2*nr),...
-        [zeros(nr,cPoints);dt*eye(cPoints);zeros(nr-cPoints,cPoints)],...
-        zeros(2*nr,1),Sist(:,1:cPoints)*ones(cPoints,1)];
-        [zeros(1,2*nr-cPoints),-dt*pIntegral(1:cPoints)/Ma, 1 ,0];
-        [zeros(1,2*nr-cPoints),-zeros(1, cPoints)  ,-dt,1]];
-    
     b = [previous_conditions.bath_surface; previous_conditions.bath_potential];
     %b = b-Sist(:,1:cPoints)*(CoM * ones(cPoints, 1)); %zs(1:cPoints)+Rv);
-    indep = [b; CoM_vel - dt/Fr - dt*F*sin(w*(t+dt)); CoM];
+    indep = [b; CoM_vel - dt/Fr - dt*F*cos(w*(t+dt)); CoM];
     
-    sol = Mat\indep;
+    % If the matrix exists, import it
+    if ~isnan(PROBLEM_CONSTANTS.precomputedInverse)
+        invMat = PROBLEM_CONSTANTS.precomputedInverse;
+        sol = invMat * indep;
+    else
+    % If it does not, create it and then save it
+        
+        We = PROBLEM_CONSTANTS.weber;
+        Re = PROBLEM_CONSTANTS.reynolds;
+        Delta = PROBLEM_CONSTANTS.laplacian;
+        DTN = PROBLEM_CONSTANTS.DTN;
+        
+        
+        pIntegral = PROBLEM_CONSTANTS.pressure_integral;
+        Ma = PROBLEM_CONSTANTS.obj_mass;
+        
+        % Preparing the matrix (2x2 block)
+        Sist = [[eye(nr)-dt*2*Delta/Re,-dt*DTN];...
+            [dt*(eye(nr)/Fr-Delta/We),eye(nr)-dt*2*Delta/Re]];
+          
+        % Completing the system
+        Mat =  [[Sist(:,(cPoints+1):2*nr),...
+            [zeros(nr,cPoints);dt*eye(cPoints);zeros(nr-cPoints,cPoints)],...
+            zeros(2*nr,1),Sist(:,1:cPoints)*ones(cPoints,1)];
+            [zeros(1,2*nr-cPoints),-dt*pIntegral(1:cPoints)/Ma, 1 ,0];
+            [zeros(1,2*nr-cPoints),-zeros(1, cPoints)  ,-dt,1]];
+        % Now save the matrix for later
+        PROBLEM_CONSTANTS.precomputedInverse = inv(Mat);
+
+        sol = Mat\indep;
+    end
+
 
     next_condition = previous_conditions;
     next_condition.bath_surface = [sol(end)* ones(cPoints, 1); ...
